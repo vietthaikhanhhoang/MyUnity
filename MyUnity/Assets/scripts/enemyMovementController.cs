0ade5347d0fb7ad779f951b122c207e5ed2ea1fc
Ba Viet <vietnbctv@gmail.com>
2022-03-10 16:17:54 +0700

    - thêm ghost: chạy theo nhân vật, bị nhân vật bắn chết

---
+++ MyUnity/Assets/scripts/enemyMovementController.cs
@@ -0,0 +1,81 @@
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovementController : MonoBehaviour
+{
+    public float enemySpeed;
+
+    Rigidbody2D enemyRB;
+    Animator enemyAnim;
+
+    //Kb cac bien de enemy co the lat duoc
+    public GameObject enemyGraphic;
+    bool facingRight = true;
+    float facingTime = 4f;
+
+    float nextFlip = 0f;
+    bool canFlip = true;
+
+    void Awake() {
+        enemyRB = GetComponent<Rigidbody2D>();
+        enemyAnim = GetComponentInChildren<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextFlip) {
+            nextFlip = Time.time + facingTime;
+            flip();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") { //enemy va cham voi player
+            if(facingRight && other.transform.position.x < transform.position.x) {
+                flip();
+            } else if(!facingRight && other.transform.position.x > transform.position.x) {
+                flip();
+            }
+
+            canFlip = false;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        if(other.tag == "Player") {
+            if(!facingRight) {
+                enemyRB.AddForce(new Vector2(-1, 0) * enemySpeed);
+            } else {
+                enemyRB.AddForce(new Vector2(1, 0) * enemySpeed);
+            }
+
+            enemyAnim.SetBool("isRun", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "Player") {
+            canFlip = true;
+            enemyRB.velocity = new Vector2(0, 0);
+            enemyAnim.SetBool("isRun", false);
+        }
+    }
+
+    void flip() {
+        if(!canFlip) return;
+
+        facingRight = !facingRight;
+        Vector3 theScale = enemyGraphic.transform.localScale;
+        theScale.x *= -1;
+        enemyGraphic.transform.localScale = theScale;
+    }
+}
